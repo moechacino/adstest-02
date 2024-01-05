f521fc80f5fe9172873cca43523f929399ff0180
mochacino <lanaksa28@gmail.com>
2024-01-05 13:41:34 +0700

    route update asset

--- routes/productAssets.js
+++ routes/productAssets.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const Router = express.Router();
-const { createAssets } = require("../controllers/productAssets");
+const {
+  createAssets,
+  deleteOneAssets,
+} = require("../controllers/productAssets");
 
 Router.route("/create").post(createAssets);
+Router.route("/delete/:id").delete(deleteOneAssets);
 
 module.exports = Router;
